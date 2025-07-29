889416a0f6060d3289ebd6df97fe585b15d9f1c7
ITWILL <ITWILL@ITWILL2-15>
2025-07-29 10:23:08 +0900

    dd

--- ILLUSION/src/main/java/com/itwillbs/illusion/controller/HomeController.java
+++ ILLUSION/src/main/java/com/itwillbs/illusion/controller/HomeController.java
@@ -21,7 +21,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 			
-		return "home";
+		return "home/home";
 	}
 	
 	
