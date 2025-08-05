6e26a9567bd6aecd3753b6e9f51433ecbc61e6e6
LEE-DAEEUN <lee0166@gmail.com>
2025-08-05 17:52:10 +0900

    채용공고 VO 작성

--- ILLUSION/src/main/java/com/itwillbs/illusion/controller/recruitment/RecruitmentController.java
+++ ILLUSION/src/main/java/com/itwillbs/illusion/controller/recruitment/RecruitmentController.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class recruitment {
-
+public class RecruitmentController {
+	
+	// 채용정보 페이지 이동
 	@GetMapping("recruitmentInfo")
 	public String recruitmentInfo() {
 		return "recruitment/recruitmentInfo";
 	}
 	
+	// 채용공고 상세페이지 이동
 	@GetMapping("recruitmentDetail")
 	public String recruitmentDetail() {
 		return "recruitment/recruitmentDetail";
